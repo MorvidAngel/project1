a512c8485b7ad2bd4332f9139173d16f390a58df
MorvidAngel <5d7n2s9m6b@live.com>
2024-06-03 18:49:12 -0400

    react project

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: '1',
+    question: 'What are accrdion components?',
+    answer: 'Accordion components are user interface elements used for organ',
+  },
+  {
+    id: '2',
+    question: 'What are accrdion components?',
+    answer: 'Accordion components are user interface elements used for organ',
+  },
+  {
+    id: '3',
+    question: 'What are accrdion components?',
+    answer: 'Accordion components are user interface elements used for organ',
+  },
+  {
+    id: '4',
+    question: 'What are accrdion components?',
+    answer: 'Accordion components are user interface elements used for organ',
+  },
+];
+
+export default data;
